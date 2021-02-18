6ea5732308d478e9d60add9a24377f06fabbe680
Stephen Moran <stephenmoran@n165-p186.eduroam.kth.se>
2021-02-18 13:42:14 +0100

    code cleanup + comments

--- hmm/hmm_2.py
+++ hmm/hmm_2.py
@@ -5,10 +5,8 @@ def div_chunks(l, n):
         yield l[i:i+n]
 
 def build_matrix(line_in):
-    #print('Line in: ', line_in)
     row = int(line_in[0])
     col = int(line_in[1])
-    #to_rtn = [[int(el)] for el in line_in[2:]]
     
     to_rtn = list(div_chunks(line_in[2:], col))
     return to_rtn
@@ -19,13 +17,7 @@ def get_lines():
         lines.append(line)
     return lines
 
-
 def read_input():
-    '''
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-    '''
-    #print('lines: ', lines[0])
     lines = get_lines()
     A = build_matrix([float(n) for n in lines[0].split()])
     B = build_matrix([float(n) for n in lines[1].split()])
@@ -34,45 +26,11 @@ def read_input():
 
     return A, B, init, seq
 
-
-'''
-#Forward algorithm
-def forward_alg(A, B, init, seq):
-    #Evaluate a0
-    init = init[0]
-    N = len(init)
-    o_0 = seq[0]
-    a0 = []
-    a0_ind
-    to_rtn = []
-    for i in range(N):
-        a0.append(init[i]*B[i][o_0])
-        a0_ind.append(-1)
-
-    to_rtn.append(a0)
-
-    #Evaluate a1 ... aT-1
-    t=1
-    for o in seq[1:]:
-        prev_a = to_rtn[t-1]
-        #Matrix multiplication
-        a = []
-        for i in range(N):
-            sum = 0
-            for j in range(N):
-                sum += prev_a[j] * A[j][i]
-            a.append(sum*B[i][o])
-        
-        to_rtn.append(a)
-        t += 1
-
-    return to_rtn
-'''
-
 #Viterbi algorithm
 def viterbi_alg(A, B, init, seq):
     #Evaluate a0
     init = init[0]
+    #N is the number of states i.e. the length of the initialisation vector
     N = len(init)
     o_0 = seq[0]
     a0 = []
@@ -82,13 +40,13 @@ def viterbi_alg(A, B, init, seq):
     to_rtn_ind = []
     for i in range(N):
         a0.append(init[i]*B[i][o_0])
+        #first state has no previous index
         a0_ind.append(-1)
 
     to_rtn.append(a0)
     to_rtn_ind.append(a0_ind)
 
     #Get most likely state
-
 
     #Evaluate a1 ... aT-1
     t=1
@@ -110,7 +68,10 @@ def viterbi_alg(A, B, init, seq):
             a.append(tmp_max*B[i][o])
             a_ind.append(tmp_m_ind)
         
+        #append alpha vector for each state to to to_rtn to create a matrix including each time step
         to_rtn.append(a)
+        #append the list which gives the most likely preceeding state for each state
+        #matrix gives this information at each time step
         to_rtn_ind.append(a_ind)
         t += 1
 
@@ -128,6 +89,7 @@ def backtrack(pred, ind):
     to_rtn = []
     prec = argmax_index(pred[-1])
     to_rtn.append(prec)
+    #start at last state and work backwards - ::-1 reverses list 
     for i in ind[::-1]:
         prec = i[prec]
         to_rtn.append(prec)
@@ -138,32 +100,9 @@ def backtrack(pred, ind):
 
 #Read the input
 A, B, init, seq = read_input()
-
-
-
-'''
-print('A: ', A)
-print('B: ', B)
-print('Init: ', init)
-print('Seq: ', seq)
-'''
-
+#Call Viterbi algorithm
 vit_lik, vit_ind = viterbi_alg(A, B, init, seq)
-
-'''
-print("Probabilities: ", vit_lik)
-print("Indeces: ", vit_ind)
-'''
-
+#backtrack to find most likely state sequence given A,B and pi
 state_seq = backtrack(vit_lik, vit_ind)
-
 print(*state_seq)
 
-
-#Sum probsabilities for the last alpha
-#print('sum: ', sum(for_alg[-1]))
-
-#with open('output.txt')
-#print(sum(for_alg[-1]))
-
-#Forward algorithm
